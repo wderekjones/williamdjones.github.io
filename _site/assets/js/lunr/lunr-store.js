4dbed05562dcc2d51af91ea6b3d86645f69f2211
Derek <derekjones2025@gmail.com>
2019-04-20 15:52:38 -0700

    deployment build 1

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,61 @@
+var store = [{
+        "title": "Our work featured at Berkeley Labs!",
+        "excerpt":"Targeting Better Cancer Drugs through Berkeley Lab Partnership ","categories": [],
+        "tags": ["research"],
+        "url": "http://localhost:4000/update-08-16-2017",
+        "teaser":null},{
+        "title": "Two Publications Accepted to AMIA 2018 Informatics Summit in San Francisco",
+        "excerpt":"","categories": [],
+        "tags": ["research"],
+        "url": "http://localhost:4000/update-12-16-2017",
+        "teaser":null},{
+        "title": "Successful defense of M.Sc thesis",
+        "excerpt":"","categories": [],
+        "tags": ["research"],
+        "url": "http://localhost:4000/update-04-12-2018",
+        "teaser":null},{
+        "title": "Poster submission accepted to ACM TAPIA 2018",
+        "excerpt":"","categories": [],
+        "tags": ["research"],
+        "url": "http://localhost:4000/update-05-04-2018",
+        "teaser":null},{
+        "title": "Started the Data Science Summer Institute internship program at LLNL",
+        "excerpt":"","categories": [],
+        "tags": ["research"],
+        "url": "http://localhost:4000/update-06-11-2018",
+        "teaser":null},{
+        "title": "Accepted ChemBio Data Scientist Position at LLNL",
+        "excerpt":"","categories": [],
+        "tags": ["research"],
+        "url": "http://localhost:4000/update-09-24-2018",
+        "teaser":null},{
+        "title": "Talk 1 on Relevant Topic in Your Field",
+        "excerpt":"This is a description of your talk, which is a markdown files that can be all markdown-ified like any other post. Yay markdown! ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/talks/2012-03-01-talk-1",
+        "teaser":null},{
+        "title": "Tutorial 1 on Relevant Topic in Your Field",
+        "excerpt":"More information here This is a description of your tutorial, note the different field in type. This is a markdown files that can be all markdown-ified like any other post. Yay markdown! ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/talks/2013-03-01-tutorial-1",
+        "teaser":null},{
+        "title": "Talk 2 on Relevant Topic in Your Field",
+        "excerpt":"More information here This is a description of your talk, which is a markdown files that can be all markdown-ified like any other post. Yay markdown! ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/talks/2014-02-01-talk-2",
+        "teaser":null},{
+        "title": "Conference Proceeding talk 3 on Relevant Topic in Your Field",
+        "excerpt":"This is a description of your conference proceedings talk, note the different field in type. You can put anything in this field. ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/talks/2014-03-01-talk-3",
+        "teaser":null},{
+        "title": "Teaching experience 1",
+        "excerpt":"This is a description of a teaching experience. You can use markdown like any other post. Heading 1 Heading 2 Heading 3 ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/teaching/2014-spring-teaching-1",
+        "teaser":null},{
+        "title": "Teaching experience 2",
+        "excerpt":"This is a description of a teaching experience. You can use markdown like any other post. Heading 1 Heading 2 Heading 3 ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/teaching/2015-spring-teaching-1",
+        "teaser":null}]
---
+++ _site/deploy.sh
@@ -0,0 +1,2 @@
+jekyll build;
+rsync -rv /home/derek/workspace/williamdjones.github.io/_site/* hobby:/u/zon-d2/grad/derek/HTML/
